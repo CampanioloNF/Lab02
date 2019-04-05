4dd099c1f0ccb0388a75330beff0d85a8afc7c88
Flavio <Flavio@Lillo>
2019-04-05 17:57:25 +0200

    modificato il metodo equals

--- Lab02_Alien/src/it/polito/tdp/alien/WordEnhanced.java
+++ Lab02_Alien/src/it/polito/tdp/alien/WordEnhanced.java
@@ -19,7 +19,7 @@ public class WordEnhanced {
 		this.translations.add(translation);
 	}
 
-
+/*
 	public boolean equals(Object o) {
 	
 		WordEnhanced altro = (WordEnhanced) o;
@@ -32,7 +32,8 @@ public class WordEnhanced {
 		
 	}
 
-
+*/
+	
 	
 	
 	public void setTranslation(String translation) {
@@ -72,6 +73,31 @@ public class WordEnhanced {
 		}
 
 		return s;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((alienWord == null) ? 0 : alienWord.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		WordEnhanced other = (WordEnhanced) obj;
+		if (alienWord == null) {
+			if (other.alienWord != null)
+				return false;
+		} else if (!alienWord.equals(other.alienWord))
+			return false;
+		return true;
 	}
 	
 	
